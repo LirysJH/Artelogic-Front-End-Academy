1c275eb2adf99da3c520ba950e87700bdb93f883
LirysJH <48212972+LirysJH@users.noreply.github.com>
2020-01-16 23:55:52 +0200

    Done writing to localStorage, styles fix, button coords fix

--- Notes/js/script.js
+++ Notes/js/script.js
@@ -1,43 +1,29 @@
-/*
-<div class="note__box">
-  <h4 class="note__label">Label</h4>
-  <div class="note__img">
-    <img class="pin-img" src="images/pin.png" alt="" />
-  </div>
-  <div class="note__text">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-      in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-      nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-      sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-  </div>
-  <div class="label-box">
-    <p>#label</p>
-  </div>
-</div>
-*/
 
 const info = {
   caption: '',
   text: '',
-  label: '',
+  label: ''
 };
+
+const pushToStorage = (key, value) => { return localStorage.setItem(key, value); };
+
+const getFromStorage = (key) => { return localStorage.getItem(key); };
 
 const button = document.getElementById('submit-btn');
 button.addEventListener("click", () =>
 {
-  info.caption = document.getElementById('label').value;
-  info.text = document.getElementById('note-text').value;
-  info.label = document.getElementById("label-select").value;
+  pushToStorage("caption", document.getElementById('label').value);
+  pushToStorage("text", document.getElementById('note-text').value);
+  pushToStorage("label", document.getElementById("label-select").value);
 
-  const content = '<div class="note__box"><h4 class="note__label">' + info.caption +
-  '</h4><div class="note__img"><img class="pin-img" src="images/pin.png" alt="" /></div>'
-  + '<div class="note__text"><p>' + info.text + '</p></div><div class="label-box"><p>'+
-  '#' + info.label.toLowerCase() +'</p></div></div>';
+  info.caption = getFromStorage("caption");
+  info.text = getFromStorage("text");
+  info.label = getFromStorage("label");
+
+  const content = `<div class="note__box"><h4 class="note__label">${info.caption}
+  </h4><div class="note__img"><img class="pin-img" src="images/pin.png" alt="" /></div>
+  <div class="note__text"><p>${info.text}</p></div><div class="label-box"><p>
+  #${info.label.toLowerCase()}</p></div></div>`;
 
   if ( info.label === '' || info.text === '' || info.label === '' )
   {
@@ -46,5 +32,10 @@ button.addEventListener("click", () =>
   else
   {
     document.getElementById("notes").innerHTML += content;
+
+    document.getElementById('label').value =
+    document.getElementById('note-text').value =
+    document.getElementById('note-text').value = "";
   }
+
 });
