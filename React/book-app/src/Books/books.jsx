044e25717c795c95284676272b2af2c3e52e2f6b
lirys <snslirys@gmail.com>
2020-02-13 16:41:00 +0200

    Changed button value to 'More'

--- React/book-app/src/Books/books.jsx
+++ React/book-app/src/Books/books.jsx
@@ -10,7 +10,7 @@ const books = props => {
                 <img src={image} alt={title} className="book-img" />
                 <a href={link} target="_blank" rel="noopener noreferrer" className="book-link">
                     <div class="to-link-button">
-                        Read
+                        More
                     </div>
                 </a>                
             </div>            
